cc6b11eba4a652e3fc54a3e3962c1ae433c47359
saitejah20 <68639223+saitejah20@users.noreply.github.com>
2022-01-07 18:45:00 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -24,7 +24,7 @@ print (cy+"Make sure you Subscribed Termux Professor On Youtube")
 print (cy+"www.youtube.com/c/TermuxProfessorYT")
 
 print (re+"NOTE :")
-print ("1. Telegram only allow to add 200 members in group by one user.")
+print ("1. Telegram only allow to add 2000 members in group by one user.")
 print ("2. You can Use multiple Telegram accounts for add more members.")
 print ("3. Add only 50 members in group each time otherwise you will get flood error.")
 print ("4. Then wait for 15-30 miniute then add members again.")
@@ -65,7 +65,7 @@ with open(r"members.csv", encoding='UTF-8') as f:  #Enter your file name
 
 chats = []
 last_date = None
-chunk_size = 200
+chunk_size = 2000
 groups = []
 
 result = client(GetDialogsRequest(
